31a19e1132d1218848a6ff118427594ddd64f7d8
Snquere <dani.mrtn@eduge.ch>
2020-01-22 13:19:35 +0100

    Ajout du freamwork UIKit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require('uikit.php');
+?>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>FBCFPTI</title>
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+
+    <body class="uk-text-center">
+
+    <body>
